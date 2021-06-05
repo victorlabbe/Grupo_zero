2e030146f732ec84066aff25bcb6965f318e8931
victorlabbe <vi.labbe@duocuc.cl>
2021-06-05 01:27:05 -0400

    agrege  el formulario nueva obra

--- js/contacto.js
+++ js/contacto.js
@@ -16,13 +16,67 @@ tinymce.init({
   });
 
 
-document.querySelector("#nuevas-form").addEventListener('submit', (e)=>{
-    e.preventDefault();
-    let archivo = document.querySelector("#formfile").getElement();
-    let titulo = document.querySelector("#titulo-txt").value;
-    let historia = document.querySelector("#historia-txt").value;
-    let descripciones = tinymce.get("descripciones-txt").getContent();
-    let tecnica = document.querySelector("#tecnica-txt").value;
-    let valor = document.querySelector("#valor-txt").value;
-    console.log("nueva imagen",archivo,titulo,historia,descripciones,tecnica,valor)
-    });
+
+  const ORegistro = [];
+
+  document.querySelector("#nuevas-form").addEventListener('submit', (e) => {
+      e.preventDefault();
+      let imagen = document.querySelector("#imagen-txt").value;
+      let titulo = document.querySelector("#titulo-txt").value;
+      let historia = document.querySelector("#historia-txt").value;
+      let descripciones = tinymce.get("descripciones-txt").getContent();
+      let tecnica = document.querySelector("#tecnica-txt").value;
+      let valor = document.querySelector("#valor-txt").value;
+      let siValido = true;
+  
+      document.querySelector("#imagen-txt").classList.remove("is-invalid");
+      document.querySelector("#titulo-txt").classList.remove("is-invalid");
+      document.querySelector("#historia-txt").classList.remove("is-invalid");
+      //document.querySelector("descripciones-txt").classList.remove("is-invalid");
+      document.querySelector("#tecnica-txt").classList.remove("is-invalid");
+      document.querySelector("#valor-txt").classList.remove("is-invalid");
+     
+  
+      
+      if (imagen.trim() == "") {
+          document.querySelector("#imagen-txt").classList.add("is-invalid");
+          siValido = false;
+      }
+      if (titulo.trim() == "") {
+          document.querySelector("#titulo-txt").classList.add("is-invalid");
+          siValido = false;
+      }
+      if (historia.trim() == "") {
+          document.querySelector("#historia-txt").classList.add("is-invalid");
+          siValido = false;
+     // }
+      //if (descripciones.trim() == "") {
+        //  document.querySelector("descripcion-txt").classList.add("is-invalid");
+          //siValido = false;
+      }
+      if (tecnica.trim() == "") {
+          document.querySelector("#tecnica-txt").classList.add("is-invalid");
+          siValido = false;
+      }
+      if (valor.trim() == "") {
+          document.querySelector("#valor-txt").classList.add("is-invalid");
+          siValido = false;
+      }
+            
+      if (siValido) {
+  
+          
+  
+          Swal.fire("Registro Confirmado", "Usuario Registrado", "success");
+  
+      }
+      document.querySelector("#imagen-txt").value = "";
+      document.querySelector("#titulo-txt").value = "";
+      document.querySelector("#historia-txt").value = "";
+      //document.querySelector("descripcion-txt").setContent("");
+      document.querySelector("#tecnica-txt").value = "";
+      document.querySelector("#valor-txt").value = "";
+      
+  
+      console.log(ORegistro);
+  });
--- js/index.js
+++ js/index.js
@@ -15,21 +15,58 @@ tinymce.init({
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
   });
 
- 
+    
   
   document.querySelector("#contacto-form").addEventListener('submit', (e)=>{
     e.preventDefault(); 
     let correo = document.querySelector("#correo-txt").value;
     let descripcion = tinymce.get("descripcion-txt").getContent();
     console.log("hola kobe",correo,descripcion);
-    
-    Swal.fire("Envio exitoso!","Su consulta fue enviada!","success");
+   
+  Swal.fire("Envio exitoso!","Su consulta fue enviada!","success");
     
   });
-
+      
       document.querySelector("#limpiar-btn").addEventListener("click", ()=>{
       document.querySelector("#correo-txt").value = "";
       tinymce.get("descripcion-txt").setContent("");
     });
 
 
+    const contactoForm = [];
+    document.querySelector("#contacto-form").addEventListener('submit', (e) => {
+        e.preventDefault();
+        let correo = document.querySelector("#correo-txt").value;
+        let descripcion = tinymce.get("descripcion-txt").getContent();        
+        let siValido = true;
+    
+        document.querySelector("#correo-txt").classList.remove("is-invalid");
+        document.querySelector("descripcion-txt").classList.remove("is-invalid");       
+    
+        
+        if (correo.trim() == "") {
+            document.querySelector("#correo-txt").classList.add("is-invalid");
+            siValido = false;
+        }
+        if (descripcion.trim() == "") {
+            document.querySelector("descripcion-txt").classList.add("is-invalid");
+            siValido = false;
+        }
+        
+        
+        if (siValido) {
+    
+            let contacto = {};
+            contacto.correo = correo;
+            contacto.descripcion = descripcion;
+                
+            contactoForm.push(contacto);
+    
+            Swal.fire("Registro Confirmado", "Usuario Registrado", "info");
+    
+        }
+        document.querySelector("#correo-txt").value = "";
+        document.querySelector("descripcion-txt").setContent("") ;        
+    
+            console.log(contactoForm);
+    });
